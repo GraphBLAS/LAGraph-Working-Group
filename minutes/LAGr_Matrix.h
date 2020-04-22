68976d2eb989ee6245c0b4450f0551e5da18986e
Tim Davis <davis@tamu.edu>
2020-04-22 09:06:26 -0500

    Apr 15 update

--- minutes/LAGr_Matrix.h
+++ minutes/LAGr_Matrix.h
@@ -1,6 +1,9 @@
 
 // LAGr_Matrix: A matrix, not a graph.
-// not opaque
+// not opaque or NOT
+
+    G->stuff = x
+    LAGr_set_stuff (G, x)
 
 typedef struct
 {
@@ -9,12 +12,31 @@ typedef struct
                         // none of the above
     symmetry            // yes, no, unknown
     diagonal            // yes, no, unknown ...
+    typeid              // enum? for now
+
+    char name [64] ;
+
+    // ...
+
+    MPI_comm_world stuff
+    FPGA stuff
+    GPU stuff ...
+    special GxB stuff
+
+    // ... other
+
+    uint8_t other [64]  // user definable space
+
+} LAGr_Matrix_struct
+
+
+typedef LAGr_Matrix *LAGr_Matrix_struct ;
 
     // discuss more:
-    typeid              // enum?
     tril or triu or neither?
     row vs col vs both (agnostic)?
     char name [64] ?
+    posweights
 
     // probably not:
     cycle
