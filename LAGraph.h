010c1f83a0389eca05f4e936957ffed16cf16eef
Tim Davis <davis@tamu.edu>
2020-07-29 10:03:57 -0500

    July 29, LAGraph.h updates

--- LAGraph.h
+++ LAGraph.h
@@ -18,6 +18,10 @@ LAGraph_Graph ;
 
 // or a different return value for error status?
 
+// TODO: discuss sanity checking.
+// what if A is unsymmetric, but the graph is labeled "undirected"?
+// error?  use A+A' ?
+
 LAGraph_Algorithm_Variant ?
 GrB_Info LAGraph_BreadthFirstSearch_variant
 or
@@ -33,6 +37,11 @@ or
     GrB_Vector level ;
     LAGraph_BreadthFirstSearch (&level, NULL, ...)
 
+//------------------------------------------------------------------------------
+// breadth first search
+//------------------------------------------------------------------------------
+
+// TODO: hops for all methods?
 
 GrB_Info LAGraph_BreadthFirstSearch     // no _Variant suffix
 (
@@ -42,7 +51,6 @@ GrB_Info LAGraph_BreadthFirstSearch     // no _Variant suffix
     // inputs:
     LAGraph_Graph G,
     GrB_Index source
-        // or GrB_Scalar source
 ) ;
 
 GrB_Info LAGraph_BreadthFirstSearch_MultiSource
@@ -52,11 +60,7 @@ GrB_Info LAGraph_BreadthFirstSearch_MultiSource
     GrB_Matrix *parent,
     // inputs:
     LAGraph_Graph G,
-    GrB_Index *sources, size_t nsources
-        // alternatives:
-        // (1)     GrB_Vector Source of size 1-by-nsources
-        //          can have different types
-        // (2) should [Source, nsources] be an LAGraph_array?
+    GrB_Index *sources, size_t nsources     // or LAGraph_array?  GrB_array?
 ) ;
 
 GrB_Info LAGraph_BreadthFirstSearch_Frontier
@@ -64,14 +68,64 @@ GrB_Info LAGraph_BreadthFirstSearch_Frontier
     // outputs:
     GrB_Vector *level,
     GrB_Vector *parent,
-    GrB_Vector *next_frontier,
-    // inputs:
-    LAGraph_Graph G,
+    // input/output:
     GrB_Vector frontier,
+    // inputs:
+    GrB_Vector vertex_mask,         // filter the vertices
+    bool vertex_mask_complement,
+    bool vertex_mask_structural,
+//  or:
+//  GrB_Descriptor desc,    // mask complement, mask structural:
+//                          // NULL, GrB_DESC_C, GrB_DESC_S, or GrB_DESC_SC
+    LAGraph_Graph G,
     uint64_t hops           // if hops=0, no limit (or INT64_MAX), or "NONE"
 ) ;
 
+//------------------------------------------------------------------------------
 // connected components:
+//------------------------------------------------------------------------------
+
+// SCC graph = Community * G * Community', a contracted graph
+
+GrB_Info LAGraph_Community_Map
+(
+    // output:
+    uint64_t &ncomponents,
+    GrB_Vector *component,  // vertex i is in component(i), a dense vector.
+    // input:
+    GrB_Matrix Community    // Community(c,i)=1 becomes c=component(i)
+) ;
+
+// Gnew = Perm * G *Perm', then Gnew is block diagonal
+GrB_Info LAGraph_Community_Permutation
+(
+    // output:
+    GrB_Matrix Perm,        // Perm(...), n-by-n
+    // input:
+    GrB_Matrix Community    // Community(c,i)=1 becomes c=component(i)
+) ;
+
+GrB_Info LAGraph_ConnectedComponents
+(
+    // output:
+    GrB_Matrix *Community,  // k-by-n if there are k components.  GrB_BOOL.
+                            // Community(c,i)=1 if c=component(i)
+    // input:
+    LAGraph_Graph G         // if directed: strongly cc
+) ;
+
+GrB_Info LAGraph_ConnectedComponents_Weakly
+(
+    // output:
+    GrB_Matrix *Community,  // k-by-n if there are k components.  GrB_BOOL.
+                            // Community(c,i)=1 if c=component(i)
+    // input:
+    LAGraph_Graph G
+) ;
+
+//------------------------------------------------------------------------------
+// centrality: up next
+//------------------------------------------------------------------------------
 
 // random number generator?  node sampler?
 
